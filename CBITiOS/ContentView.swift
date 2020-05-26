25b149b84d5452cad2b2415058ce985d9122543b
akhilanandsirra <akhilanand9999@gmail.com>
2020-05-26 16:42:21 +0530

    fonts added

--- CBITiOS/ContentView.swift
+++ CBITiOS/ContentView.swift
@@ -7,15 +7,44 @@
 //
 
 import SwiftUI
-
+let gradStart=Color(red: 25.0/255, green: 170.0/255, blue: 179.0/255)
+let gradStop=Color(red: 35.0/255, green: 167.0/255, blue: 216.0/255)
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [gradStart, gradStop]), startPoint: .leading, endPoint: .trailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                user()
+                CBITStudentLogin()
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CBITStudentLogin: View {
+    var body: some View {
+        Text("CBIT Student Login")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundColor(Color.white)
+            .padding(.bottom, 20)
+    }
+}
+
+struct user: View {
+    var body: some View {
+        Image("user")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 75, height: 75)
+            .padding(.bottom, 0)
     }
 }
