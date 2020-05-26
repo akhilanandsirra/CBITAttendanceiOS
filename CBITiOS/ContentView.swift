326fc1afe2fc6d721227cece4b41ddd464720fbb
akhilanandsirra <akhilanand9999@gmail.com>
2020-05-26 15:15:00 +0530

    Initial Commit

---
+++ CBITiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CBITiOS
+//
+//  Created by Akhil Anand Sirra on 26/05/20.
+//  Copyright © 2020 Akhil Anand Sirra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CBITiOSTests/CBITiOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  CBITiOSTests.swift
+//  CBITiOSTests
+//
+//  Created by Akhil Anand Sirra on 26/05/20.
+//  Copyright © 2020 Akhil Anand Sirra. All rights reserved.
+//
+
+import XCTest
+@testable import CBITiOS
+
+class CBITiOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
